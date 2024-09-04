07a2409c7b9fc252595a87716b935b9d8211a672
rakshamishra54 <152586656+rakshamishra54@users.noreply.github.com>
2024-09-04 19:37:24 +0530

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -10,7 +10,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request..get('email')
+        email = request.form.get('email')
         password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
