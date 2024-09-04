97c70cce76a6c8fa5ed64989fd695f947a43de95
raksha <rakshamishra54@gmail.com>
2024-09-04 19:24:28 +0530

    Testing

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-
+# I am checking changes
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
